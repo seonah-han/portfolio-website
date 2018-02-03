736556bb5afd032e2344bae0f83437dadb04f392
seonah-han <seonah.svg@gmail.com>
2018-02-04 04:15:17 +0900

    logo, nav-mini fixed, home, contact details, in process of lab page

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,70 @@
 $(function(){
+/*================================
+				COMMON
+================================*/ 
+	$.get("/header.html", function(data){
+		$('#common-container').prepend(data);
+		var browser=navigator.userAgent.toLowerCase();
+		if(!(browser.indexOf('firefox') > -1)){
+			var wholeItems = $('#logo-svg-items');
+			var upperTag = $('#upper-tag');
+			var lowerTag = $('#lower-tag');
+			var slash = $('#slash');
+			var text = $('#logo-svg-text');
+
+			TweenMax.set(text, { x: 300});
+
+			var tl = new TimelineMax();
+			tl.pause();
+
+			tl.to('#logo-svg', 0, {
+				attr: {
+					viewBox:'0 0 700 300'
+				}
+				})
+			  .to(wholeItems, 0.2, {
+				transformOrigin: "50% 50%",
+				x: 5,
+				rotation: -90,
+				})
+			  .to(lowerTag, 0.2, {
+			  	y: 400 
+				})
+			  .to(slash, 0.2, {
+			  	x: -15,
+			  	y: 400,
+			  	rotation: -20,
+				}, "-=0.2")
+			  .to(text, 0.2, {
+				x:0,
+				opacity:1
+				}, "-=0.1")
+
+			$('#logo-svg').mouseenter(function(){
+				tl.play();
+			});
+			$('#logo-svg').mouseleave(function(){
+				tl.reverse();
+			});			
+		}
+
+		$("#nav-mini").on("click", function(){
+			$('#nav').toggleClass("nav-modal");
+			$(this).toggleClass("nav-close");
+
+		});
+
+		/* order of appearance*/
 
 
+	});
+
+	$.get("/footer.html", function(data){
+		$('#common-container').append(data);
+	});
+/*================================
+				HOME
+================================*/
 	infiniteLoop(); //put because of 3000 delay in setInterval
 	setInterval(infiniteLoop, 4000); 
 	function infiniteLoop(){ 
@@ -22,23 +86,95 @@ $(function(){
 				ease:Power0.easeNone,
 				// ease:Linear.easeOut
 			}); 
+
 		});
+
 	};
 
-	$("#nav-mini").on("click", function(){
-		$('#nav').toggleClass("nav-modal");
-		$(this).toggleClass("nav-close");
 
-	});
+/*================================
+				CONTACT
+================================*/
 
-	$('#lab-list a').click(function(e){
-		var filename = $(this).attr('id');
+
+
+
+/*================================
+				LAB
+================================*/
+
+
+	$('#lab-list li').on('click', function(e){
+		var filename = $(this).find('a').attr('id');
 		$('#lab-content').load("/lab/lab-list/"+filename+".html");
+	
+		$('#lab-list li').removeClass("lab-hover-click");
+		
+		$(this).addClass("lab-hover-click");
+
+	// 	$('#lab-list li').css({
+	// 		'border-color':'var(--color-gallery-heading)',
+	// 		'color':'var(--color-gallery-heading)'
+	// 	});
+	// 	$(this).css({
+	// 		'border-color':'var(--color-blue)',
+	// 		'color':'var(--color-blue)'
+	// 	})
 		e.preventDefault();
+	// })
+	// .hover(function(){
+	// 	$(this).css({
+	// 		// 'border-color':'var(--color-blue)',
+	// 		'color':'var(--color-blue)'
+	// 	})		
+	// },function(){
+	// 	$(this).css({
+	// 		// 'border-color':'var(--color-gallery-heading)',
+	// 		'color':'var(--color-gallery-heading)'
+	// 	})		
+
+
+	})
+	.hover(function(){
+		$(this).addClass('lab-hover-click');
+	},function(){
+		$(this).removeClass('lab-hover-click');
 	});
+
+
+
+
+/*================================
+				ABOUT
+================================*/
+
+/*================================
+				PORTFOLIO
+================================*/
+
 
 	$('#portfolio-container a').click(function(e){
 		$('#gallery-modal').load("/portfolio/portfolio-list/portfolio-1.html").show();
 		e.preventDefault();
-	})
+	});
+
+
+
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
